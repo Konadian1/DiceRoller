49546d327eb556a4db0a37055d5f5e45cb06b0fc
Konadian1 <konadian1@gmail.com>
2019-02-06 09:51:40 -0500

    v2 working

--- src/diceroller/DiceRollerv2.java
+++ src/diceroller/DiceRollerv2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class DiceRollerv2 {
 
-    /* use case to determine dice type followed by number of dice, loop until "no more" is entered
+    /* use case to determine dice type followed by number of dice, 
     then roll all the dice using math.random to determine numbers.
     
     NEXT STEP: allow multiple dice.
@@ -16,41 +16,50 @@ public class DiceRollerv2 {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-        int num=0,type=0,total=0, bonus=0, e=0;
-        Boolean Val= false;
+        int num=0,type=0,total=0;
         String Dice;
-        String NumOfRoll[];
-        String TypeOfRoll[];
         String Roll[];
         System.out.println("Dice roller!");
-        do{
-            System.out.println("Please enter the dice you would like to roll! ex 2d6 or 1d20. You can enter 'no more' to end : ");
-            Dice = input.nextLine();
-            while(Val==false){
-                if (Dice.matches("[\\d]{1,3}d[\\d]{1,3}")){
-                    Val=false;
-                    Roll[e]=Dice;
-                    e++;
-                }
-                
-                else if(Dice.matches("no more")){
-                    Val=true;
-                }
-                
-                else{
-                    System.out.println("Please enter a valid choice! ex: 4d4 or 2d100 or 'no more' to end: ");
-                    Val=false;
-                    Dice = input.nextLine();
-                }
+            System.out.println("Please enter the dice you would like to roll! ex 2d6 or 1d20. You can include multiple dice and modifiers : ");
+            try{
+                Dice = input.nextLine();
+                Dice = Dice.replaceAll("\\s","");
+                System.out.println("you roll: "+ Parse(Dice));
+            } catch(ArithmeticException ex) {
+                System.out.println("invalid dice type. Please enter the dice you would like to roll: ");
+                Dice= input.nextLine();
+                Dice = Dice.replaceAll("\\s","");
+                Parse(Dice);
             }
-            
-            
-            Val=false;                
-            Roll= Dice.split("d");
-            num[e]=Integer.parseInt(NumOfRoll[e]);
-            type[e]=Integer.parseInt(TypeOfRoll[e]);
-            
-            switch(type){
+    }
+    
+    
+    
+    
+    
+    public static int Parse(String toParse)throws ArithmeticException{
+        if (toParse.length()<1){
+            return 0;
+        }
+        else if(toParse.length()<6){
+        try {
+            int parsed = Integer.parseInt(toParse);
+            return parsed;
+        } catch (NumberFormatException e) {
+            if(toParse.matches("[\\d]{1,3}d[\\d]{1,3}")){
+                return Roller(toParse);
+            }
+        }
+    }
+        String sp[] = toParse.split("\\+", 2);
+        String l = sp[0]; 
+        String r = sp[1];
+        return Parse(l)+Parse(r);
+    }
+    public static int Roller(String toParse)throws ArithmeticException{
+        String nums[]= toParse.split("d");
+        int typeDice= Integer.parseInt(nums[1]), numDice= Integer.parseInt(nums[0]);
+        switch(typeDice){
                 case 4:
                 case 6:
                 case 8:
@@ -58,26 +67,39 @@ public class DiceRollerv2 {
                 case 12:
                 case 20:
                 case 100:
-                    Val=true;
                     break;
                 default:
-                    Val=false;
-                    break;
-            } 
-        }while (Val==false);
-        
-        System.out.println("would you like to add any modifiers for your roll? Enter 0 for none:");
-        bonus= input.nextInt();
-        
-        
-    int temp = 0;
-    while(num>0){
-    temp = (int)Math.ceil(Math.random()*type);
-    total += temp;
-    System.out.print(temp + " ");
-    num--;   
+                    throw new ArithmeticException("invalid type");
+            }
+        int total=0;
+        int num= numDice;
+        while(num>0){
+                    
+            int temp = (int)Math.ceil(Math.random()*typeDice);
+            total += temp;
+            num--;   
     }
-    int totalb= total+bonus;
-    System.out.println(": " + totalb);
+        System.out.println("your " + numDice+ " d"+typeDice+"'s rolled " +total);
+        return total;
     }
 }
+
+/*function parse(expr) {
+  var parsed = parseInt(expr);
+  if(!isNaN(parsed)) { //Check if is a number
+    return parsed;
+  } else if(expr.match(X"d"Y)) { //Check if dice
+    return roll(expr);
+  }
+
+  //Else is a '+' operation
+  var sp = expr.split("+");
+  var l = sp[0]; 
+  var r = sp[1];
+  return parse(l) + parse(r);
+}
+
+function roll(diceString) {
+  return 1; //TODO: fix me!
+}*/
+
